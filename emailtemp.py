d61003414d0a8ab1e93707708d57f0949730686b
marcd6 <marc.dumantay@gmail.com>
2016-05-17 20:25:03 -0500

    work plz

---
+++ emailtemp.py
@@ -0,0 +1,13 @@
+import requests
+import bs4
+import datetime
+
+current_date = datetime.datetime.now()
+#print(current_date)
+current_year = current_date.year
+
+
+print('hello world')
+#print(requests.codes.ok) #equal to 200
+#use .raise_for_status() method to check for errors
+
