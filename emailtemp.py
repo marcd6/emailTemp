15e6d5b5c1a74b0cb050300aa4a1c8779046eb85
marcd6 <marc.dumantay@gmail.com>
2016-05-18 19:12:03 -0500

    first commit

--- emailtemp.py
+++ emailtemp.py
@@ -3,9 +3,16 @@ import bs4
 import datetime
 
 current_date = datetime.datetime.now()
-#print(current_date)
-current_year = current_date.year
+print(current_date)
+#current_year = current_date.year
 
+#one_day = datetime.timedelta(days=1)
+#print(current_date + one_day)
+
+for day_delta in range(1, 6):
+    day_add = datetime.timedelta(days=day_delta)
+    future_date = current_date + day_add
+    print(future_date)
 
 print('hello world')
 #print(requests.codes.ok) #equal to 200
